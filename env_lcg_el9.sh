82ee211e101a805c9e66a5ae48e3f56efa56625d
Oliver Rieger <oliver.rieger@cern.ch>
2024-09-26 12:45:17 +0200

    update for EL9

--- env_lcg_el9.sh
+++
@@ -1,4 +0,0 @@
-. /cvmfs/sft.cern.ch/lcg/views/LCG_104/x86_64-el9-gcc11-opt/setup.sh
-export PATH=${PATH}:${PWD}/build/bin
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PWD}/build/lib
-export PYTHONPATH=${PYTHONPATH}:${PWD}/build/lib/python
